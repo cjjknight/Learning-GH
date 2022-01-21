1a7def2c3c55d7da0ca5add62be39bbdd4388366
Chris Johnson <Christopher.Johnson42@gmail.com>
2022-01-21 14:35:30 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by user210522 on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
